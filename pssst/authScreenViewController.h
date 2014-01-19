79a12e5b7b57bfa327014cef23ddc5515363d9b0
Aaron <aaronaug@umich.edu>
2014-01-19 04:18:33 -0500

    +custom class for authentication screen

---
+++ pssst/authScreenViewController.h
@@ -0,0 +1,13 @@
+//
+//  authScreenViewController.h
+//  pssst
+//
+//  Created by Aaron Augsburger on 1/19/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface authScreenViewController : UIViewController
+
+@end
---
+++ pssst/authScreenViewController.m
@@ -0,0 +1,13 @@
+//
+//  authScreenViewController.m
+//  pssst
+//
+//  Created by Aaron Augsburger on 1/19/14.
+//
+//
+
+#import "authScreenViewController.h"
+
+@implementation authScreenViewController
+
+@end
